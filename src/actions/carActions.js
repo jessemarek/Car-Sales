c833c1872260da0d1ee84d6782ea1b5c69828b54
unknown <dethgrenade@gmail.com>
2020-05-12 12:28:47 -0700

    initial commit. store setup, reducer created mSTP implemented

---
+++ src/actions/carActions.js
@@ -0,0 +1,14 @@
+export const ADD_FEATURE = 'ADD_FEATURE'
+export const addFeature = () => {
+    return { type: ADD_FEATURE }
+}
+
+export const REMOVE_FEATURE = 'REMOVE_FEATURE'
+export const removeFeature = () => {
+    return { type: REMOVE_FEATURE }
+}
+
+export const UPDATE_PRICE = 'UPDATE_PRICE'
+export const updatePrice = () => {
+    return { type: UPDATE_PRICE }
+}
